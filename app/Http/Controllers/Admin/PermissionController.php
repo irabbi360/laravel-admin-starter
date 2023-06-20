dde00910aa88a2546027e10b695b473601b886b0
Fazle Rabbi <35403788+irabbi360@users.noreply.github.com>
2023-06-20 09:50:29 +0600

    permission  controller added

---
+++ app/Http/Controllers/Admin/PermissionController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Http\Requests\StorePermissionRequest;
+use App\Http\Requests\UpdatePermissionRequest;
+use Illuminate\Support\Facades\Gate;
+use Spatie\Permission\Models\Permission;
+
+class PermissionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        abort_if(Gate::denies('permission_access'), Response::HTTP_FORBIDDEN, '403 Forbidden');
+
+        $permissions = Permission::all();
+
+        return view('admin.permissions.index', compact('permissions'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        abort_if(Gate::denies('permission_create'), Response::HTTP_FORBIDDEN, '403 Forbidden');
+
+        return view('admin.permissions.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StorePermissionRequest $request)
+    {
+        $permission = new Permission();
+        $permission->name = $request->name;
+
+        if ($permission->save()) {
+            toast('Permission successfully created!','success');
+            return redirect()->route('admin.permissions.index');
+        }
+        toast('Whoops! Permission create failed', 'error');
+        return redirect()->back();
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Permission $permission)
+    {
+        abort_if(Gate::denies('permission_show'), Response::HTTP_FORBIDDEN, '403 Forbidden');
+
+        return view('admin.permissions.show', compact('permission'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Permission $permission)
+    {
+        abort_if(Gate::denies('permission_edit'), Response::HTTP_FORBIDDEN, '403 Forbidden');
+
+        return view('admin.permissions.edit', compact('permission'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(StorePermissionRequest $request, Permission $permission)
+    {
+        $permission->update($request->all());
+        toast('Permission successfully updated!','success');
+        return redirect()->route('admin.permissions.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Permission $permission)
+    {
+        abort_if(Gate::denies('permission_delete'), Response::HTTP_FORBIDDEN, '403 Forbidden');
+
+        $permission->delete();
+
+        return back();
+    }
+}
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->integer('status')->default(1);
             $table->rememberToken();
             $table->timestamps();
         });
